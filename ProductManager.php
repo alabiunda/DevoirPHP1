705e948b3c55aa8dc0a762073d494021978cd3b4
Alexandre <la_biunda.0108.student@ifosupwavre.be>
2020-04-20 15:43:59 +0200

    V1

--- ProductManager.php
+++ ProductManager.php
@@ -1,5 +1,10 @@
 <?php
     class ProductManager{
+        private $results = array();
+        private $conndb;
 
+        public function __construct($host,$dbname,$id,$pw){
+            $this->conndb = new PDO('mysql:'.$host);
+        }
     }
 ?>
