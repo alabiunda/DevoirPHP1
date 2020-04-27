7365e3b9ed889089100a5ae5b61a766f4e9d5ae4
Alexandre La Biunda <la_biunda.0108.student@ifosupwavre.be>
2020-04-27 12:14:17 +0200

    Dump DB

--- dump/demo.sql
+++ dump/demo.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Mar 30, 2020 at 03:56 PM
--- Server version: 10.4.10-MariaDB
--- PHP Version: 7.3.12
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  lun. 27 avr. 2020 à 10:13
+-- Version du serveur :  5.7.23
+-- Version de PHP :  7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,28 +19,36 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `demo`
+-- Base de données :  `demo`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `log`
+-- Structure de la table `log`
 --
 
 DROP TABLE IF EXISTS `log`;
 CREATE TABLE IF NOT EXISTS `log` (
   `pk` int(11) NOT NULL AUTO_INCREMENT,
   `action` varchar(255) NOT NULL,
-  `ts` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`pk`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
+--
+-- Déchargement des données de la table `log`
+--
+
+INSERT INTO `log` (`pk`, `action`, `ts`) VALUES
+(4, 'test', '2020-04-20 21:09:01'),
+(5, 'test', '2020-04-26 19:54:18'),
+(6, 'test', '2020-04-26 19:54:35');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `products`
+-- Structure de la table `products`
 --
 
 DROP TABLE IF EXISTS `products`;
@@ -53,22 +61,25 @@ CREATE TABLE IF NOT EXISTS `products` (
   `price_total` float NOT NULL,
   `quantity` int(11) NOT NULL,
   PRIMARY KEY (`pk`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `products`
+-- Déchargement des données de la table `products`
 --
 
 INSERT INTO `products` (`pk`, `name`, `price`, `vat`, `price_vat`, `price_total`, `quantity`) VALUES
-(1, 'Super Smash Bros Ultimate', 42, 21, 0, 42, 8),
+(1, 'Super Smash Bros Ultimate', 40, 21, 8.4, 48.4, 11),
 (2, 'Tekken 4', 20, 6, 1.2, 21.2, 12),
 (3, 'The Witcher 3', 100, 10, 10, 110, 2),
-(4, 'World Of Warcraft', 50, 0, 0, 50, 21),
-(5, 'Warcraft 3', 0, 0, 0, 0, 9),
-(6, 'Farm Simulator', 0, 0, 0, 0, 2);
+(4, 'World Of Warcraft', 30, 0, 0, 30, 21),
+(5, 'Warcraft 3', 10, 0, 0, 10, 9),
+(6, 'Farm Simulator', 5, 0, 0, 5, 2),
+(18, 'Test', 25, 0, 0, 0, 4),
+(19, 'Test', 25, 0, 0, 0, 4),
+(23, 'Test', 123, 0, 0, 0, 1234);
 
 --
--- Triggers `products`
+-- Déclencheurs `products`
 --
 DROP TRIGGER IF EXISTS `prod_vat`;
 DELIMITER $$
@@ -89,7 +100,7 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Structure de la table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
@@ -97,22 +108,23 @@ CREATE TABLE IF NOT EXISTS `users` (
   `pk` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`pk`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `users`
+-- Déchargement des données de la table `users`
 --
 
 INSERT INTO `users` (`pk`, `username`, `password`, `created_at`, `updated_at`) VALUES
-(1, 'johndoe', '12345678', '2020-03-12 17:40:36', '2020-03-12 17:40:36'),
+(1, 'johndoe', '123456789', '2020-04-20 20:55:01', '2020-04-20 20:55:01'),
 (2, 'admin', 'password123', '2020-03-11 20:30:47', '2020-03-11 20:30:47'),
-(4, 'ramon', '123456', '2020-03-12 17:16:40', '2020-03-12 17:16:40');
+(4, 'ramon', '123456', '2020-03-12 17:16:40', '2020-03-12 17:16:40'),
+(9, 'test', '123456', '2020-04-26 19:54:18', '2020-04-26 19:54:18');
 
 --
--- Triggers `users`
+-- Déclencheurs `users`
 --
 DROP TRIGGER IF EXISTS `log_crea_users`;
 DELIMITER $$
